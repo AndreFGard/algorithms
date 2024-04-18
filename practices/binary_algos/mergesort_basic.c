4153c7a0f8a1b3236ab1311a10a6a41dc22d1185
AndreFGard <andrefsagh@gmail.com>
2024-04-18 20:13:29 -0300

    create mergesort file

---
+++ practices/binary_algos/mergesort_basic.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+
+typedef long int li;
+
+int mergesort(li *nums, int start, int end);
+
+
+int main(){
+
+    //li num_n = 0;
+    li num_n = 0;
+    printf("input: ");
+    scanf("%ld", &num_n);
+    printf("num of nums is %ld\n", num_n);
+
+}
+
+int mergesort(li *nums, int start, int end){
+
+    return 0;
+}
