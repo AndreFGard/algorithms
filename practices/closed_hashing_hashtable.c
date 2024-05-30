a2a83b0108246a4b49d698b973289654198d507e
AndreFGard <andrefsagh@gmail.com>
2024-05-30 17:26:06 -0300

    add random probing to closed hashing hashtable

--- practices/closed_hashing_hashtable.c
+++ practices/closed_hashing_hashtable.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
-#define tablesize_start 11
+#define tablesize_start 8
 #define previously_busy 2
 #define empty  1
 #define full 0
 
-
+int tablesize = 8;
 typedef struct slot_t {
     unsigned char stat;
     int value;
@@ -20,7 +21,7 @@ typedef struct hashtable {
 } hashtable;
 
 int hash(int value, int size){
-    return (value*value)%size;
+    return value - (size * floor(value/size));
 }
 
 
@@ -32,7 +33,7 @@ void populate_table_arr(hashtable *t, slot_t *arr){
 
 //linear probing conflict resolution
 int insert_l(hashtable *t, int value) {
-    int tablesize = t->max_size;
+    tablesize = t->max_size;
     int k = hash(value, tablesize);
     slot_t *s = &t->table[k];
     if (s->stat != full){
@@ -72,10 +73,10 @@ int insert_l(hashtable *t, int value) {
 
 
 int find_l(hashtable *t, int value) {
-    int tablesize = t->max_size;
+    tablesize = t->max_size;
     int k = hash(value, tablesize);
     int i = k;
-    
+
     int found_at = -1;
     do {
         if (t->table[i].stat != empty) {
@@ -84,7 +85,7 @@ int find_l(hashtable *t, int value) {
                 found_at = i; 
                 break;
             }
-            i++;
+            i = (i+1)%tablesize;
         }
         else {
             break;
@@ -95,7 +96,7 @@ int find_l(hashtable *t, int value) {
 }
 
 int remove_l(hashtable *t, int value){
-    int tablesize = t->max_size;
+    tablesize = t->max_size;
     int idx = find_l(t, value);
     if (idx != -1){
         t->curr_size -=1;
@@ -113,7 +114,7 @@ void populate_table(hashtable *t){
     populate_table_arr(t, t->table);
 }
 
-int main() {
+int test_linear() {
     hashtable table;
     populate_table(&table);
     for (int i = 0; i < 13; i++){
@@ -127,7 +128,92 @@ int main() {
         if (find_l(&table, i) == -1){
             printf("couldnt find value %d\n", i);
         }
-    }  
-
+    } 
+        populate_table(&table);
+    for (int i = 0; i < 13; i++){
+        insert_l(&table, i);
+    }
+    for (int i = 0; i < 13; i++){
+        if (find_l(&table, i) == -1){
+            printf("couldnt find value %d\n", i);
+        }
+    } 
     puts("eba, salvei e guardei tudo!");
+}
+
+
+/////random probing hashtable
+
+int randints[] = {0, 2, 6, 7, 3, 1, 4, 5};
+int insert_r(hashtable *t, int value) {
+    tablesize = t->max_size;
+    int k = hash(value, tablesize);
+
+    if (t->curr_size == t->max_size){
+        puts("n cabe nao"); return -1;
+    }else
+
+    //this for does reach every position, please note, because the randints
+    //are all between 0 and tablesize and have no repetition
+    for (int i = 0; i<tablesize; i++){
+        if (t->table[   (k + randints[i])%tablesize  ].stat != full){
+            k = (k + randints[i])%tablesize;
+            //one could check if we arent repeating an item right here
+            t->table[k].value = value;
+            t->table[k].stat = full;
+            t->curr_size++;
+            return k;
+        }
+    }
+
+}
+
+int find_r(hashtable *t, int value){
+    tablesize = t->max_size;
+    int k = hash(value, tablesize);
+
+    //this for does reach every position, please note, because the randints
+    //are all between 0 and tablesize and have no repetition
+    for (int i = 0; i<tablesize; i++){
+        if (t->table[   (k + randints[i])%tablesize  ].stat != empty){
+            
+            //one could check if we arent repeating an item right here
+            if (t->table[   (k + randints[i])%tablesize  ].value == value && t->table[   (k + randints[i])%tablesize  ].stat != previously_busy){
+                k = (k + randints[i])%tablesize;
+                return k;
+            }
+        }
+        else{
+            // nao esta aqui, bye bye
+            break;
+        }
+    }    
+
+    return -1;
+}
+
+
+int main(){
+    hashtable table;
+    populate_table(&table);
+
+    hashtable *t = &table;
+
+
+
+    int nums[] = {2, 4, 8, 16, 32, -12,78};
+    for (int i = 0; i < 6; i++){
+        insert_r(&table, nums[i]);
+    }
+
+    for (int i = 0; i <7; i++){
+        if (find_r(t, nums[i]) == -1){
+            printf("nao achei %d\n", nums[i]);
+        }
+    }
+
+    for (int i = 0; i < 8; i++){
+        printf("%d at %d\n",table.table[i].value, i);
+    }
+    puts("eae");
 }
