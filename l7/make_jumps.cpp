8baad6ce96d680273bb5331d75e265b20da811ec
AndreFGard <96950645+AndreFGard@users.noreply.github.com>
2024-07-30 11:10:50 -0300

    finish make jumps
    is it even backtracking or is it only dynamic programming?

--- l7/make_jumps.cpp
+++ l7/make_jumps.cpp
@@ -22,7 +22,7 @@ unordered_set<pair<int,int>, pair_hash> que;
 vector<vector<int>> m;
 vector<pair<int,int>> specs;
 
- int valid(int x, int y){
+int valid(int x, int y){
     if((y>-1) && (y < specs.size()))
         if ((x >= specs[y].first) && (x<= specs[y].second))
             return 1;
@@ -30,8 +30,8 @@ vector<pair<int,int>> specs;
     
 }
 
-int rowChange[] = {-2, -2, -1, -1, 1, 1, 2,2};
 int colChange[] = {-1, 1, -2, 2, -2, 2, -1,1};
+int rowChange[] = {-2, -2, -1, -1, 1, 1, 2,2};
 
 //this is 100% flawed, should be erased.
 // i just realized that the statement says that a spot should not be visited twice
@@ -68,12 +68,40 @@ int bfs(int startx, int starty){
     return 1;
 }
 
+int newtraversal(int stx, int sty, int reached){
+    int oldreached =  m[sty][stx];
+    if (!m[sty][stx]) 
+        m[sty][stx] = 1;
+    m[sty][stx] *= -1; //////change this
+    int maxxer = 0;
+
+    for (int comb = 0; comb<8; comb++){
+        int nc = colChange[comb] + stx;
+        int ny = rowChange[comb] + sty;
+
+        if (valid(nc, ny)){
+            //check if it's not in our recursion:
+            if (m[ny][nc] > -1){
+                int t = newtraversal(nc,ny, 0);
+                maxxer = (t >= maxxer) ? t+1 :maxxer;
+                //m[ny][nc] *= -1;
+            }
+        }
+    }
+
+    //return m[sty][stx] = max(oldreached, maxxer);
+    m[sty][stx] = max(oldreached, maxxer);
+    return maxxer;
+    
+}
+
 
 
 
 
 int main(){
     int n;
+    int casecount = 1;
     scanf("%d",&n);
     while (n){
 
@@ -85,12 +113,14 @@ int main(){
             scanf("%d %d", &startx, &nel);
 
             area += nel;
-            m[i].resize(10, unvisited);
+            m[i].resize(10, 0);
             specs[i] = make_pair(startx,startx + nel-1);
         }
-
-        bfs(specs[0].first, 0);
-        printf("%d\n", area - que.size());
+        int difference = area - 1 - newtraversal(specs[0].first, 0, 0);
+        if (difference == 1)
+            printf("Case %d, %d square can not be reached.\n",casecount++,difference);
+        else printf("Case %d, %d squares can not be reached.\n",casecount++,difference);
+        //printf("%d\n", area - que.size());
         que.clear();
 
         scanf("%d",&n);
