34385b1ca2c0bd76098313030b229ca9456db79c
AndreFGard <96950645+AndreFGard@users.noreply.github.com>
2024-07-30 09:26:46 -0300

    finish spiky_maze.cpp

--- l7/spiky_maze.cpp
+++ l7/spiky_maze.cpp
@@ -12,39 +12,93 @@ const char wall = '#' * (-1);
 const char open = '.' * (-1);
 const char start = '@' * (-1);
 const char treasure = 'x' * (-1);
-vector<vector<int>> m;
+vector<vector<char>> m;
+vector<vector<char>> mback;
 vector<vector<int>> stor;
 int rows, cols, spikejumps;
 
+// i dont code like this usually, sorry
+int backfs(int xo, int yo, int hearts);
+
+
+
 int bfs(int xo, int yo, int hearts){
+    int result = 0;
+
     if (m[yo][xo] == spike) hearts--;
     else if(m[yo][xo] == treasure){
-        return hearts;
+        result = backfs(xo,yo,hearts);
+        if (result) return result;
+        return 0;
     }
 
-    if(!hearts) return 0;
+    if(hearts<0) return 0;
 
     int bak = m[yo][xo];
     m[yo][xo] = visited;
-    int result = 0;
-    if (((xo -1)>0)&&(m[yo][xo-1] != visited)&&(m[yo][xo-1] != wall)){
+
+    if (((xo -1)>=0)&&(m[yo][xo-1] != visited)&&(m[yo][xo-1] != wall)){
         result = bfs(xo-1,yo,hearts);
-        if (result) return result;
+        if (result){ m[yo][xo] = bak; return result;}
 
     }
 
     if(((xo +1)<cols)&&(m[yo][xo+1] != visited)&&(m[yo][xo+1] != wall)){
         result = bfs(xo+1,yo,hearts);
-        if (result) return result;
+        if (result){ m[yo][xo] = bak; return result;}
     }
 
     if((yo+1 < rows)&&(m[yo+1][xo] != visited)&&(m[yo+1][xo] != wall)){
         result = bfs(xo,yo+1,hearts);
-        if (result) return result;
+        if (result){ m[yo][xo] = bak; return result;}
     }
+
+    if((yo-1 >= 0)&&(m[yo-1][xo] != visited)&&(m[yo-1][xo] != wall)){
+        result = bfs(xo,yo-1,hearts);
+        if (result){ m[yo][xo] = bak; return result;}
+    }
+
     m[yo][xo] = bak;
     return 0;
 }
+
+int backfs(int xo, int yo, int hearts){
+    if (mback[yo][xo] == spike) hearts--;
+    else if(mback[yo][xo] == start){
+        return hearts+1;
+    }
+
+    if(hearts<0) return 0;
+
+    int bak = mback[yo][xo];
+    mback[yo][xo] = visited;
+    
+    int result = 0;
+    if (((xo -1)>=0)&&(mback[yo][xo-1] != visited)&&(mback[yo][xo-1] != wall)){
+        result = backfs(xo-1,yo,hearts);
+        if (result){ m[yo][xo] = bak; return result;}
+
+    }
+
+    if(((xo +1)<cols)&&(mback[yo][xo+1] != visited)&&(mback[yo][xo+1] != wall)){
+        result = backfs(xo+1,yo,hearts);
+        if (result){ mback[yo][xo] = bak; return result;}
+    }
+
+    if((yo+1 < rows)&&(mback[yo+1][xo] != visited)&&(mback[yo+1][xo] != wall)){
+        result = backfs(xo,yo+1,hearts);
+        if (result){ m[yo][xo] = bak; return result;}
+    }
+
+    if((yo-1 >= 0)&&(mback[yo-1][xo] != visited)&&(mback[yo-1][xo] != wall)){
+        result = backfs(xo,yo-1,hearts);
+        if (result){ m[yo][xo] = bak; return result;}
+    }
+
+    mback[yo][xo] = bak;
+    return 0;
+}
+
 
 
 
@@ -53,23 +107,27 @@ int main(){
     scanf("%d %d %d", &rows,&cols,&spikejumps);
     queue<pair<int,int>> doors;
     m.resize(rows);
+    mback.resize(rows);
     stor.resize(rows);
     for ( int i = 0; i<rows;i++){
         stor[i].resize(cols, unvisited);
         char line[50];
-        scanf("%s", &line);
+        scanf("%s", line);
         //why didnt i use chars?
         for (int c = 0;c<cols;c++){
             if (((-1) * line[c]) == start)
                 doors.push(make_pair(i,c));
             m[i].push_back((-1) * line[c]);
+            mback[i].push_back((-1) * line[c]);
         }
     }
 
     while(!doors.empty())
         {
             auto t = doors.front();doors.pop();
-            printf("%d\n", bfs(t.second,t.first, spikejumps));}
-
-
+            if (bfs(t.second,t.first, spikejumps))
+               { printf("%s\n", "SUCCESS" );return 0;}
+                
+        }
+    printf("%s\n", "IMPOSSIBLE");
 }
